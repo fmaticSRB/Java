1dd8ccd20ec4c9e119a6151404c4239077a365b6
fmaticSRB <51976766+fmaticSRB@users.noreply.github.com>
2019-06-19 02:29:23 -0700

    Added proper header info

--- hello_world.java
+++ hello_world.java
@@ -1,14 +1,14 @@
 /*********************************************************
- * This is a basic "Hello World" Code for Practice Purposes
+ * Author: fmaticSRB 
+ * Description: This is a basic "Hello World" Code for Practice Purposes
  *
- * Copyright (c) 2019 fmaticSRB
- * All rights reserved
  *********************************************************/
 public class HelloWorld {
 
     public static void main(String[] args) {
+    	
         /* This code prints out a line of code and then goes to the next line */
-        System.out.println("Hello, World.");
+        System.out.println("Hello, World.");	
         System.out.println("Hello, Java.");
     }
 }
