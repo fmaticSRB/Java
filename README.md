7cf50f10f51c2a42451696fc04e71217a54108ae
fmaticSRB <51976766+fmaticSRB@users.noreply.github.com>
2019-06-18 18:03:16 -0700

    nothing

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hello_world
 First Test Repo
+
+Contains a basic Hello world program in Java
---
+++ hello_world.java
@@ -0,0 +1,15 @@
+/*********************************************************
+ * This is a basic "Hello World" Code for Practice Purposes
+ *
+ * Copyright (c) 2019 fmaticSRB
+ * All rights reserved
+ *********************************************************/
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
+
