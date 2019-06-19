887fb7577f2fbfd882d0a3a6defce5d5d504c7d0
fmaticSRB <51976766+fmaticSRB@users.noreply.github.com>
2019-06-19 16:20:39 -0700

    Added basic description and comments

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # BankAccount_2
-This account is a simple account that uses multiple classes
+This program simulates the user accessing their bank account. 
+Unlike BankAccount_1, this program has only one account and 
+one type of account (removed checkings, savings, etc). 
+
+However, this program utilizes multiple objects. This is done
+in order to display how to utlize multiple objects and how
+to access them. 
---
+++ UserAccount.java
@@ -0,0 +1,112 @@
+ /***********************************************************
+ * AUTHOR: fmaticSRB
+ * Title: UserAccount
+ * SPECIFICATION: This program deals with the exact details
+ * pertaining to the users bank acount. 
+ ************************************************************/
+
+import java.text.NumberFormat;
+
+public class UserAccount
+{
+	NumberFormat fmt = NumberFormat.getCurrencyInstance();
+
+	// Instance variables
+	private String ID;
+	private double Balance;
+
+	// Constructors & Methods
+
+	// Instantiates the values for ID and the Balacne
+	public UserAccount()
+	{
+		ID = "???";
+		Balance = 0;
+	}
+
+	// Sets the ID and Balance to initial values
+	public UserAccount(String initID, double initBalance)
+	{
+		ID = initID;
+		Balance = initBalance;
+	}
+
+	public UserAccount(String initID)
+	{
+		ID = initID;
+	}
+
+	// Returns the ID of the account
+	public String getID()
+	{
+		return ID;
+	}
+
+	// Returns the balance of the account
+	public double getBalance()
+	{
+		return Balance;
+	}
+
+	// Sets the ID of the account
+	public void setID(String name)
+	{
+		ID = name;
+	}
+
+	// Credits the account with a deposit as long as the conditions are met
+	public void deposit(double depositAmount)
+	{
+		if (depositAmount > 0)
+		{
+			Balance = depositAmount + Balance;
+		}
+	}
+
+	// Credits the account with a withdrawl as long as the conditions are met
+	public boolean withdraw(double withdrawAmount)
+	{
+		if (withdrawAmount >0 && withdrawAmount <= Balance)
+		{
+			Balance = Balance - withdrawAmount;
+			return true;
+		}
+		else
+		return false;
+	}
+
+	// Adds interest to the account based on the amount of money in the account
+	public void addInterest()
+	{
+		if (Balance > 5000)
+		{
+			Balance = (Balance * .045) + Balance;
+		}
+		else if(Balance > 1000)
+		{
+			Balance = (Balance * .035) + Balance;
+		}
+		else
+		{
+			Balance = (Balance * .025) + Balance;
+		}
+	}
+
+	// Checks to see if the ID's of two different accounts are the same
+	public boolean equals(UserAccount otherAccount)
+	{
+		// local variables
+		boolean flag = false;
+		if(getID().equals(otherAccount.getID()))
+		{
+			flag = true;
+		}
+		return flag;
+	}
+
+	// Returns the information about the account (ID and Balance)
+	public String toString()
+	{
+		return "ID: " + ID + "\nBalance: " + fmt.format(Balance);
+	}
+} // End Class
